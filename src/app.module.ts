fa821225607cc921dbec701b31dffceb8a5fde11
Patryk.budnicki <patryk.budnicki@siili.com>
2023-01-16 21:44:54 +0100

    feat: add graphql to application and enable graphql plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // zeby cala aplikacja mogla korzystać z graphqla, musimy dodać:
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // miejsce w którym będzie się generowała schema
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
