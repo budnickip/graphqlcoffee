d21efc0ca0d3a39889c61c65ef2fdaa09f50964c
Patryk.budnicki <p-budnicki@wp.pl>
2023-01-17 19:43:37 +0100

    feat: add docker and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,16 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     // zeby cala aplikacja mogla korzystać z graphqla, musimy dodać:
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
